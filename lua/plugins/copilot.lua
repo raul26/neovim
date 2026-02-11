4df67356f46909fcc7f124258277f03d1291e960
Raul Vargas <kvm2694@gmail.com>
2026-02-10 20:06:43 -0600

    Updated plugins.

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,36 +1,36 @@
 return {
-  {
-    "github/copilot.vim",
-    cmd = "Copilot",
-    -- build = "<cmd>Copilot setup<cr>",
-    event = "BufWinEnter",
-    init = function()
-      vim.g.copilot_no_maps = true
-      vim.g.copilot_filetypes = {
-        ["*"] = true,
-        gitcommit = false,
-        NeogitCommitMessage = false,
-        DressingInput = false,
-        TelescopePrompt = false,
-        ["neo-tree-popup"] = false,
-        ["dap-repl"] = false,
-      }
-    end,
-    config = function()
-      -- Block the normal Copilot suggestions
-      -- This setup and the callback is required for
-      -- https://github.com/fang2hou/blink-copilot
-      vim.api.nvim_create_augroup("github_copilot", { clear = true })
-      for _, event in pairs({ "FileType", "BufUnload", "BufEnter" }) do
-        vim.api.nvim_create_autocmd({ event }, {
-          group = "github_copilot",
-          callback = function()
-            vim.fn["copilot#On" .. event]()
-          end,
-        })
-      end
-    end,
-  },
+  -- {
+  --   "github/copilot.vim",
+  --   cmd = "Copilot",
+  --   -- build = "<cmd>Copilot setup<cr>",
+  --   event = "BufWinEnter",
+  --   init = function()
+  --     vim.g.copilot_no_maps = true
+  --     vim.g.copilot_filetypes = {
+  --       ["*"] = true,
+  --       gitcommit = false,
+  --       NeogitCommitMessage = false,
+  --       DressingInput = false,
+  --       TelescopePrompt = false,
+  --       ["neo-tree-popup"] = false,
+  --       ["dap-repl"] = false,
+  --     }
+  --   end,
+  --   config = function()
+  --     -- Block the normal Copilot suggestions
+  --     -- This setup and the callback is required for
+  --     -- https://github.com/fang2hou/blink-copilot
+  --     vim.api.nvim_create_augroup("github_copilot", { clear = true })
+  --     for _, event in pairs({ "FileType", "BufUnload", "BufEnter" }) do
+  --       vim.api.nvim_create_autocmd({ event }, {
+  --         group = "github_copilot",
+  --         callback = function()
+  --           vim.fn["copilot#On" .. event]()
+  --         end,
+  --       })
+  --     end
+  --   end,
+  -- },
   {
     "olimorris/codecompanion.nvim",
     dependencies = {
@@ -85,8 +85,4 @@ return {
       },
     },
   },
-  -- {
-  --   "MeanderingProgrammer/render-markdown.nvim",
-  --   ft = { "markdown", "codecompanion" },
-  -- },
 }
